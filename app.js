2efdd6c090d865057958ef8b035f7ef2d6d45507
wangminghuan <m.h.wang@foxmail.com>
2020-11-18 15:55:37 +0800

    feat:增加反向代理与spa应用中间件

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
+const { historyApiFallback } = require('koa2-connect-history-api-fallback');
 const logger = require('koa-logger')
+const proxy = require('koa-server-http-proxy')
 const index = require('./routes/index')
-
+// 代理接口
+// app.use(proxy('/api', {
+//   target: 'https://m.toutiao.com',
+//   changeOrigin: true,
+//   pathRewrite: {
+//     '^/api': '/'
+//   }
+// }));
+// SPA单页应用模板全部指向 '/'
+// app.use(historyApiFallback({index:"/", whiteList: ['/api'] }));
 // error handler
 onerror(app)
 
@@ -14,10 +25,11 @@ onerror(app)
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
+
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
 
+app.use(require('koa-static')(__dirname + '/public'))
 app.use(views(__dirname + '/views', {
   extension: 'html'
 }))
@@ -29,9 +41,12 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
 // 调用router.routes()来组装匹配好的路由，返回一个合并好的中间件
 // 调用router.allowedMethods()获得一个中间件，当发送了不符合的请求时，会返回 `405 Method Not Allowed` 或 `501 Not Implemented`
-app.use(index.routes()).use(index.allowedMethods())
+app.use(index.routes())
+.use(index.allowedMethods())
+
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
